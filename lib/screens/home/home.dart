361517151fcdb9d56dc63967b76130b816c7eeb0
epitchi <thienvanlea1@gmail.com>
2021-02-08 10:23:01 +0700

    done bottom tab bar

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,22 +1,16 @@
+import 'package:delayed_display/delayed_display.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify_ui/components/app_bar.dart';
+import 'package:spotify_ui/components/tab_bar.dart';
 import 'package:spotify_ui/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: buildAppBar(context, title: 'Chào buổi sáng!'),
-      bottomNavigationBar: Container(
-        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20,),
-        margin: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-        decoration: new BoxDecoration(
-          color: kTabBarColor,
-          borderRadius: BorderRadius.circular(80.0)
-        )
-      )
-    );
+        appBar: buildAppBar(context, title: 'Chào buổi sáng!'),
+        bottomNavigationBar: CustomTabBar());
   }
 }
 
