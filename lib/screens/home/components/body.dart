e48731e69b3b9eb64a45d3d191da3c8af0fcc1b3
epitchi <thienvanlea1@gmail.com>
2021-02-10 03:14:18 +0700

    Update readme img

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,9 +1,5 @@
-import 'package:delayed_display/delayed_display.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify_ui/components/title.dart';
-import 'package:spotify_ui/constants.dart';
-import 'package:spotify_ui/models/PlayList.dart';
 
 import 'explore_button.dart';
 import 'my_play_lists.dart';
